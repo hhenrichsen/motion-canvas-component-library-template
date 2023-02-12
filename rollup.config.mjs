1e2499477db1ee55021ff052f12b3e917fdf2ceb
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-02-11 23:18:22 -0700

    :tada: Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,37 @@
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import jsx from 'acorn-jsx';
+
+/** @type {import('rollup').RollupOptions} */
+const config = [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/bundle.js',
+      format: 'umd',
+      name: 'MotionCanvasNord',
+    },
+    acornInjectPlugins: [jsx()],
+    plugins: [typescript()],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/index.js',
+      format: 'es',
+    },
+    acornInjectPlugins: [jsx()],
+    plugins: [typescript()],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/index.d.ts',
+      format: 'es',
+    },
+    acornInjectPlugins: [jsx()],
+    plugins: [dts()],
+  },
+];
+
+export default config;
